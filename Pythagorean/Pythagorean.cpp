58ce07a76d6150f66321a18d6e46562d16a66068
Vincent <vdterpst@gmail.com>
2019-09-26 21:07:13 -0400

    Added Pythagorian triples

---
+++ Pythagorean/Pythagorean.cpp
@@ -0,0 +1,55 @@
+/**
+ * Vincent Terpstra
+ * 26 September 2019
+ * 
+ * A program that finds all pythagorean triples with hypotenus less then input or MAX_VALUE
+ *   a pythagorian triple has values a^2 + b^2 = c^2 where a, b, c are integers
+ * 
+ * g++ *.cpp ../Bitmap.cpp -std=c++17 && ./a.out
+ */
+
+#include <iostream>
+#include <functional>
+#include <math.h>
+
+#include "../Bitmap.h"
+
+using namespace std;
+
+#define MAX_VALUE 1000
+
+int main(int size, char* args[]){
+    int  max = MAX_VALUE;
+    try {
+        if(size > 1)
+            max = atoi(args[1]);
+    } catch(...){}
+
+    int found(0);
+    Bitmap black("black.bmp", max, max);
+        black.fill(0);
+    for(int hyp = 1; hyp <= max; hyp++){
+        for(int a = 1; a < hyp; ++a){
+            for(int b = 1; b < a; ++b){
+                if(a * a + b * b == hyp * hyp ){
+                    cout << "[ "<< b << ", " << a << ", " << hyp <<"]" << endl;
+                    float re = 255 * (float)a / hyp;
+                    float gr = 255 * (float)b / hyp;
+                    float bl = 255 * (float)hyp / max;
+                    black.add(a, b, re, gr, bl);
+                    black.add(b, a, gr, re, bl);
+                    ++found;
+                }
+            }
+        }
+    }
+    cout << found << " triples found!" << endl;
+    
+
+}
+
+
+
+
+
+
