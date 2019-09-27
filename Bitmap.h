4597cab47aba7aa4e0ad043b3c3c35202593ebda
Vincent <vdterpst@gmail.com>
2019-09-26 21:22:45 -0400

    removed folder

---
+++ Bitmap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Bitmap {
+    unsigned char * image;
+    
+    const char * filename;
+    int idx = 0;
+    public:
+        const int height;
+        const int width;
+        Bitmap(const char * filename, const int height, const int width);
+
+        ~Bitmap();
+
+        void operator<<(float input);
+        void fill(float clr);
+        void add(int x, int y, float r, float g, float b);
+};
